e52c175345fffb59bfc510d4540e7bf074895a6b
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-04-01 14:02:24 +0700

    feat: comment forgot password page to create new feature

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+/* 'use client'
 
 import React, { useState } from "react";
 import Link from "next/link";
@@ -92,3 +92,4 @@ export default function ForgotPassword() {
         </div>
     );
 }
+ */
